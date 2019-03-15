d5630e09787aff7ada5d60f81bacdecfc868f9f2
Michael Vodyanyy <m.vod@skbkontur.ru>
2019-03-15 13:26:14 +0500

    Move compression to its own class. Move Pixel components initialization to constructor in switch

--- JPEG/DCT.cs
+++ JPEG/DCT.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.CodeDom;
-using JPEG.Utilities;
 
 namespace JPEG
 {
--- JPEG/Images/Pixel.cs
+++ JPEG/Images/Pixel.cs
@@ -1,44 +1,38 @@
-﻿using System;
-using System.Linq;
-
-namespace JPEG.Images
+﻿namespace JPEG.Images
 {
     public class Pixel
     {
-        private readonly PixelFormat format;
-
         public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
         {
-            format = pixelFormat;
             switch (pixelFormat)
             {
                 case PixelFormat.RGB:
-                    r = firstComponent;
-                    g = secondComponent;
-                    b = thirdComponent;
+                    R = firstComponent;
+                    G = secondComponent;
+                    B = thirdComponent;
+
+                    Y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+                    Cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+                    Cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
                     break;
                 case PixelFormat.YCbCr:
-                    y = firstComponent;
-                    cb = secondComponent;
-                    cr = thirdComponent;
+                    Y = firstComponent;
+                    Cb = secondComponent;
+                    Cr = thirdComponent;
+
+                    R = (298.082 * Y + 408.583 * Cr) / 256.0 - 222.921;
+                    G = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+                    B = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
                     break;
             }
         }
 
-        private readonly double r;
-        private readonly double g;
-        private readonly double b;
+        public readonly double R;
+        public readonly double G;
+        public readonly double B;
 
-        private readonly double y;
-        private readonly double cb;
-        private readonly double cr;
-
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
-
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public readonly double Y;
+        public readonly double Cb;
+        public readonly double Cr;
     }
 }
